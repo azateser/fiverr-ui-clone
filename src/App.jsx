9f53e0e36d46a2b62fc1e180ed7d0fcba98e53a6
Azat ESER <azatesser@gmail.com>
2023-02-25 00:39:20 +0300

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
 
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Footer from "./components/footer/Footer";
 import Gigs from "./pages/gigs/Gigs";
@@ -9,38 +9,21 @@ import "./App.scss";
 import NotFound from "./pages/404/NotFound";
 
 function App() {
-  const Layout = () => {
-    return (
-      <div className="app">
-        <Navbar />
-        <Outlet />
-        <Footer />
-      </div>
-    );
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/fiverr-ui-clone/",
-      element: <Layout />,
-      children: [
-        {
-          path: "/fiverr-ui-clone/dist/",
-          element: <Home />,
-        },
-        {
-          path: "/fiverr-ui-clone/dist/gigs",
-          element: <Gigs />,
-        },
-      ],
-    },
-    {
-      path: "*",
-      element: <NotFound />,
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  const path = useLocation().pathname;
+  console.log(path);
+  return (
+    <div className="App">
+      {path === "/" && <Navbar />}
+      {path === "/gigs" && <Navbar />}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/gigs" element={<Gigs />} />
+        <Route exact path="*" element={<NotFound />} />
+      </Routes>
+      {path === "/" && <Footer />}
+      {path === "/gigs" && <Footer />}
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./assets/reset.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
